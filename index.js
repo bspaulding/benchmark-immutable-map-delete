5faf3323ad34e1667ca8b87e2a926017460f8272
Bradley Spaulding <brad.spaulding@gmail.com>
2016-08-23 16:13:44 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const Benchmark = require("benchmark");
+const { fromJS, Map } = require("immutable");
+
+function makeData() {
+	var data = new Map({});
+	for (var i = 0; i < 10000; i += 1) {
+		data = data.set(i, i);
+	}
+	return data;
+}
+
+function makeKeys(n) {
+	var xs = [];
+	for (var i = 0; i < n; i += 1) {
+		xs.push("" + n);
+	}
+	return xs;
+}
+
+const data = makeData();
+
+const suite = new Benchmark.Suite();
+
+const nKeys = [100, 1000, 5000, 10000];
+
+nKeys.forEach(function(n) {
+	suite.add("Map#delete n=" + n, function() {
+		var result = data;
+		for (var i = 0; i < n; i += 1) {
+			result = result.delete(i);
+		}
+	});
+
+	suite.add("Map#toJS/fromJS n=" + n, function() {
+		var result = data.toJS();
+		for (var i = 0; i < n; i += 1) {
+			result[i] = undefined;
+		}
+		result = fromJS(result);
+	});
+
+	var keys = makeKeys(n);
+	suite.add("Map#filter n=" + n, function() {
+		var result = data.filter(function(value, key) {
+			return keys.indexOf(key) < 0;
+		});
+	});
+});
+
+suite.on('cycle', function(event) {
+  console.log(String(event.target));
+});
+
+suite.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+});
+
+suite.run({ 'async': true });
